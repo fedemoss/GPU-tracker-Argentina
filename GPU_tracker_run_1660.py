d42e0376da1488a82aa4843692152c78d7b1819d
Federico Moss <54726553+fedemoss@users.noreply.github.com>
2022-02-18 22:29:04 -0300

    Add files via upload

---
+++ GPU_tracker_run_1660.py
@@ -0,0 +1,7 @@
+import GPU_tracker_argentina as GPU
+#my situation
+PATH = r'C:\Users\mossney\Documents\Selenium\chromedriver.exe'
+df_1660 = GPU.price_tracker('1660 super', 80000, PATH, False , 6)
+
+
+
---
+++ GPU_tracker_run_580.py
@@ -0,0 +1,5 @@
+import GPU_tracker_argentina as GPU
+#my situation
+PATH = r'C:\Users\mossney\Documents\Selenium\chromedriver.exe'
+df_580 = GPU.price_tracker('580', 80000, PATH, False , 60)
+
